892fd7fb42ed19352b607e67810df994af87b77a
Ritu252 <66035353+Ritu252@users.noreply.github.com>
2020-05-28 16:02:16 +0530

    positive numbers in a range of numbers
    start and end is taken as input

---
+++ postive numbers in a range.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#making range of numbers by taking starting and ending number as input
+st = int(input("enter the first number:"))
+end = int(input("enter the second number:"))
+for i in range(st,end+1):   #for loop runs until it change each and every number in the range
+    if i>0:             #if statement checks whether the given condition satisfies
+        print(i)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
